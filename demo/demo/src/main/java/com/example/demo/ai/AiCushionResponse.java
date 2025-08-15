05b465eb9b3a61a4916316411e6d03abbdb7e348
Kangboom <jkl6147@naver.com>
2025-08-15 13:29:42 +0900

    feat; ai 쿠션어 응답 dto 생성

---
+++ demo/demo/src/main/java/com/example/demo/ai/AiCushionResponse.java
@@ -0,0 +1,11 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AiCushionResponse {
+    
+    private String result;
+}
