19674c3993bb854df78d4817cad085a2d095c6d9
Kangboom <jkl6147@naver.com>
2025-08-16 14:35:51 +0900

    feat: 필요없는 코드 삭제

---
+++ demo/demo/src/main/java/com/example/demo/ai/AiConventionRequest.java
@@ -0,0 +1,33 @@
+package com.ssafy.ottereview.ai.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@NoArgsConstructor  
+@AllArgsConstructor                                                                            
+@Getter
+public class AiConventionRequest {
+    @JsonProperty("repo_id")
+    private Long repoId;
+    private String source;
+    private String target;
+    private Rules rules;
+
+    @Getter
+    @Builder
+    public static class Rules {
+        @JsonProperty("file_names")
+        private String fileNames;
+        @JsonProperty("function_names")
+        private String functionNames;
+        @JsonProperty("variable_names")
+        private String variableNames;
+        @JsonProperty("class_names")
+        private String classNames;
+        @JsonProperty("constant_names")
+        private String constantNames;
+    }
+}
